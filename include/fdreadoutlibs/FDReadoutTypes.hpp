7ad2ba242d69057345efa5405dc678f7f43ff16e
Alessandro Thea <alessandro.thea@cern.ch>
2022-10-07 23:57:00 +0200

    Working on fdreadouttypes names

--- include/fdreadoutlibs/FDReadoutTypes.hpp
+++ include/fdreadoutlibs/FDReadoutTypes.hpp
@@ -1,11 +1,11 @@
-#include "ProtoWIBSuperChunk.hpp"
-#include "DUNEWIBSuperChunk.hpp"
+#include "ProtoWIBSuperChunkOverlay.hpp"
+#include "DUNEWIBSuperChunkOverlay.hpp"
 #include "DAPHNESuperChunk.hpp"
-#include "TDEAMC.hpp"
-#include "SSPFrame.hpp"
-#include "SW_ProtoWIB_TriggerPrimitive.hpp"
-#include "SW_DUNEWIB_TriggerPrimitive.hpp"
-#include "RawFW_WIB_TriggerPrimitive.hpp"
+#include "TDEAMCFrameOverlay.hpp"
+#include "SSPFrameOverlay.hpp"
+#include "ProtoWIBTriggerPrimitiveOverlay.hpp"
+#include "DUNEWIBTriggerPrimitiveOverlay.hpp"
+#include "RawFWDUNEWIBTriggerPrimitiveSuperChunkWrapper.hpp"
 
 
 #include "FDReadoutTypes_old.hpp"
