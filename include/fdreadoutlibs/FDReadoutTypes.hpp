dd5a71cecd08d4ab8b412444cda9369f37283b6b
Alessandro Thea <alessandro.thea@cern.ch>
2022-10-12 16:39:31 +0200

    Another round of re-naming

--- include/fdreadoutlibs/FDReadoutTypes.hpp
+++ include/fdreadoutlibs/FDReadoutTypes.hpp
@@ -1,11 +1,10 @@
-#include "ProtoWIBSuperChunkOverlay.hpp"
-#include "DUNEWIBSuperChunkOverlay.hpp"
-#include "DAPHNESuperChunk.hpp"
-#include "TDEAMCFrameOverlay.hpp"
-#include "SSPFrameOverlay.hpp"
-#include "ProtoWIBTriggerPrimitiveOverlay.hpp"
-#include "DUNEWIBTriggerPrimitiveOverlay.hpp"
-#include "RawFWDUNEWIBTriggerPrimitiveSuperChunkWrapper.hpp"
+#include "ProtoWIBSuperChunkTypeAdapter.hpp"
+#include "DUNEWIBSuperChunkTypeAdapter.hpp"
+#include "DAPHNESuperChunkTypeAdapter.hpp"
+#include "TDEAMCFrameTypeAdapter.hpp"
+#include "SSPFrameTypeAdapter.hpp"
+#include "TriggerPrimitiveTypeAdapter.hpp"
+#include "RawFWDUNEWIBTriggerPrimitiveSuperChunkTypeAdapter.hpp"
 
 
 #include "common.hpp"
---
+++ include/fdreadoutlibs/VariableSizePayloadTypeAdapter.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file FDReadoutTypes.hpp Common types in udaq-readout
+ *
+ * This is part of the DUNE DAQ , copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+#ifndef FDREADOUTLIBS_INCLUDE_FDREADOUTLIBS_FDREADOUTTYPES_HPP_
+#define FDREADOUTLIBS_INCLUDE_FDREADOUTLIBS_FDREADOUTTYPES_HPP_
+
+#include <cstdint> // uint_t types
+#include <memory>  // unique_ptr
+#include <vector>
+#include <cstring> // memcpy
+#include <tuple> // tie
+
+namespace dunedaq {
+namespace fdreadoutlibs {
+namespace types {
+
+
+/**
+ * @brief Convencience wrapper to take ownership over char pointers with
+ * corresponding allocated memory size.
+ * */
+struct VariableSizePayloadWrapper
+{
+  VariableSizePayloadWrapper() {}
+  VariableSizePayloadWrapper(size_t size, char* data)
+    : size(size)
+    , data(data)
+  {}
+
+  size_t size = 0;
+  std::unique_ptr<char> data = nullptr;
+};
+
+} // namespace types
+} // namespace fdreadoutlibs
+} // namespace dunedaq
+
+#endif // FDREADOUTLIBS_INCLUDE_FDREADOUTLIBS_FDREADOUTTYPES_HPP_
